23b06d24bd94a1df16e3bf9ac3ae1c9c5fce9044
Brayan Arellano <brayanarellano1605@gmail.com>
2024-05-17 10:12:16 -0500

    Cambios empleado

--- src/main/java/com/syntaxerror/seminario/model/Empleado.java
+++ src/main/java/com/syntaxerror/seminario/model/Empleado.java
@@ -10,16 +10,29 @@ public class Empleado {
     @Column(name = "empleado_id")
     private Long empleadoId;
 
+    @Column(name = "usuario_id", nullable = false)
+    private  Long usuarioId;
+
     @Column(name = "unidad_id", nullable = false)
     private Long unidadId;
+
+    @Column(name = "cargo", nullable = false, length = 50)
+    private String cargo;
+
+    @Column(name = "status", columnDefinition = "BOOLEAN DEFAULT TRUE")
+    private boolean status;
 
     // Getters and setters
     public Long getEmpleadoId() {
         return empleadoId;
     }
 
-    public void setEmpleadoId(Long empleadoId) {
-        this.empleadoId = empleadoId;
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
     }
 
     public Long getUnidadId() {
@@ -28,5 +41,19 @@ public class Empleado {
 
     public void setUnidadId(Long unidadId) {
         this.unidadId = unidadId;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
--- src/main/java/com/syntaxerror/seminario/repository/EmpleadoRepository.java
+++ src/main/java/com/syntaxerror/seminario/repository/EmpleadoRepository.java
@@ -4,8 +4,14 @@ import com.syntaxerror.seminario.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long>{
-    //findByUserId
+    //Find an employee by ID
     Empleado findByEmpleadoId(Long empleadoId);
+    //Find all employees by user
+    List<Empleado> findByUsuarioId(Long userId);
+    //Find all employees of a service unit
+    List<Empleado> findByUnidadId(Long serviceUnitID);
 }
