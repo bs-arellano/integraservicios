71ef6be4aa567f6558bc4900b43b78c167aa8f82
Brayan Arellano <brayanarellano1605@gmail.com>
2024-05-25 17:52:29 -0500

    cambio cors

--- src/main/java/com/syntaxerror/seminario/config/SecurityConfig.java
+++ src/main/java/com/syntaxerror/seminario/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.syntaxerror.seminario.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -12,7 +17,20 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/**");
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                            CorsConfiguration configuration = new CorsConfiguration();
+                            configuration.applyPermitDefaultValues();
+                            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                            return configuration;
+                        }
+                ))
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        .requestMatchers("/**").permitAll()
+                );
+        return http.build();
+
     }
 }
